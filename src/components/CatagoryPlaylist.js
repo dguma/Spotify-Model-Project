74a9459bc1eb681732257267e70b51524c74b3aa
dguma <dylan.guma@gmail.com>
2021-10-17 22:28:16 -0400

    added playlist items and links

--- src/components/CatagoryPlaylist.js
+++ src/components/CatagoryPlaylist.js
@@ -1,10 +1,59 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
+import styled from 'styled-components';
+
+const Thumbnail = styled.img`
+    width: 50%;
+    height: 50%;
+`;
+
+const PlaylistContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
+    flex-direction: row;
+    width: 100%;
+
+    .contentContainer {
+        width: 25%;
+        height: 25%;
+
+        .innerContent {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
+    }
+`;
+
+const PlaylistListContainer = styled.div`
+    width: 100%;
+    height: 25vh;
+    overflow-y: scroll;
+    color: white;
+
+    ul {
+        display: flex;
+        justify-content: flex-start;
+        flex-direction: column;
+        align-items: center;
+        flex-wrap: wrap;
+        list-style-type: none;
+
+        li {
+            padding: 1em;
+        }
+    }
+
+`;
 
 function CatagoryPlaylist(props) {
 
 const [catagory, setCatagory] = useState([]);
 const [playlists, setPlaylists] = useState();
+const [playlistContent, setPlaylistContent] = useState([]);
 
 useEffect(() => {
     const url = 'https://accounts.spotify.com/api/token';
@@ -24,30 +73,67 @@ useEffect(() => {
         headers: { 'Authorization': 'Bearer ' + data.access_token}
         })
         .then(res => res.json())
-        .then(data => setPlaylists(data.playlists.items))
+        .then(data => {
+            setPlaylists(data.playlists.items)
+        })
     })
     
 }, [])
 
 console.log(playlists)
+console.log(playlistContent)
 
 let {id} = useParams();
+
+function handler(event) {
+    event.preventDefault();
+    console.log(event.target.id)
+    fetch(event.target.id , {
+        method: 'GET',
+        headers: { 'Authorization': 'Bearer ' + props.token}
+        })
+        .then(res => res.json())
+        .then(data => {
+            setPlaylistContent(data.items)
+        })
+    
+}
 
     return (
         <div>
            <h1>{id}</h1>
 
+           <PlaylistListContainer>
+                <ul>
+                    {playlistContent.map((data) => {
+                        return (
+                            <a href={data.track.preview_url} >
+                                <li>
+                                    {data.track.name}
+                                    <audio src={data.track.preview_url}/>
+                                </li>
+                                </a>
+                            
+                        )
+                    })}
+                </ul>
+          </PlaylistListContainer>
+           
+            <PlaylistContainer>
            {
            (playlists !== undefined) ?
            playlists.map(data => {
                return (
-                   <div>
-                       <img src={data.images[0].url} />
-                       <p>{data.name}</p>
+                   <div className='contentContainer'>
+                        <div className='innerContent'>
+                            <p>{data.name}</p>
+                            <Thumbnail onClick={handler} id={data.tracks.href} src={data.images[0].url} />
+                        </div>
                    </div>
                )
            }) : ''
         }
+        </PlaylistContainer>
         </div>
     );
 }
